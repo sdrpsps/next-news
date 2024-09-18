0485680ecb6499dfe59da331362dea9c4a6beb0c
sunny <sunny@bytespark.app>
2024-09-18 13:39:10 +0800

    catch-all route

--- app/archive/@archive/[year]/page.js
+++
@@ -1,9 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function FilteredNewsPage({ params }) {
-  const year = params.year;
-  const news = getNewsForYear(year);
-
-  return <NewsList news={news} />;
-}
